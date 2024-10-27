d59cd0a3d05f347b0de1068c662caf49e9bc429c
Shashwot KC <49743516+shashwot2@users.noreply.github.com>
2024-10-27 17:46:22 -0400

    Fix readbooks with more fields

--- read_booksh5.py
+++ read_booksh5.py
@@ -9,17 +9,33 @@ with h5py.File(file_path, 'r') as h5file:
 
     print("\nBooks Data:")
     for book_key in h5file.keys():
-        if 'volumeInfo' in h5file[book_key]:
-            volume_info = h5file[f'{book_key}/volumeInfo']
+        book_group = h5file[book_key]
+
+        if 'volumeInfo' in book_group:
+            volume_info = book_group['volumeInfo']
 
             title = volume_info.attrs.get('title', 'No Title Found')
             print("\nTitle:", title)
 
             authors = []
-            for key in volume_info.keys():
+            for key in volume_info:
                 if key.startswith('authors_'):
-                    authors.append(volume_info[key][()])
+                    author = volume_info[key][()].decode('utf-8') 
+                    authors.append(author)
 
-            print("Authors:", authors)
+            print("Authors:", ', '.join(authors))
+
+            published_date = volume_info.get('publishedDate', 'N/A')
+            if isinstance(published_date, h5py.Dataset):
+                published_date = published_date[()].decode('utf-8')  
+            print("Published Date:", published_date)
+
+            categories = []
+            for key in volume_info:
+                if key.startswith('categories_'):
+                    category = volume_info[key][()].decode('utf-8')
+                    categories.append(category)
+
+            print("Categories:", ', '.join(categories))
         else:
             print(f"\nNo 'volumeInfo' found in '{book_key}'.")
